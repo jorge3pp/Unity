15bc9623df97d7bef1cae614a3025f9adadfec8b
Jorge Poveda Pérez <jorge23cs@gmail.com>
2018-11-14 18:14:05 +0000

    Hay un error que el profe tiene q explicar

---
+++ AI/Assets/Scripts/AiFollowPath.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiFollowPath : MonoBehaviour {
+
+    public PathMarker Target;
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        Move();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Move()
+    {
+        agent.destination = Target.transform.position;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PathMarker")
+        {
+            //Target = other.gameObject.GetComponent<PathMarker>();
+            //Target = other.gameObject.GetComponent<PathMarker>().NextMarker;
+            Target = Target.NextMarker;
+            Move();
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "PathMarker")
+        {
+            Target = Target.NextMarker;
+            Move();
+        }
+    }
+
+
+}
---
+++ AI/Assets/Scripts/PathMarker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMarker : MonoBehaviour {
+    //retrieved by the AI when it collides with a PathMarker
+    public PathMarker NextMarker;
+
+    private void OnDrawGizmos()
+    {
+        if (NextMarker != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, NextMarker.transform.position);
+        }
+    }
+}
